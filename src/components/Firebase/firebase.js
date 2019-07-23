34ab0ae7d292cd5f4bcaa60bbf82d207e7a012ed
Greg C <gconrad0922@gmail.com>
2019-07-23 15:43:33 -0400

    Removed API Key and placed in .env

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/database';
 
 
 const config = {
-    apiKey: "AIzaSyDRrjtu3ofVbUflbDgunVNFyctGDw8OwAA",
-    authDomain: "react-baby-feeding.firebaseapp.com",
-    databaseURL: "https://react-baby-feeding.firebaseio.com",
-    projectId: "react-baby-feeding",
-    storageBucket: "",
-    messagingSenderId: "250094831011",
-    appId: "1:250094831011:web:3a32e955b0dec6c7"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 class Firebase {
